978e4e8b6fcf36b3e6f786ea3cd0eff3a754a89d
fadill314 <58606938+fadill314@users.noreply.github.com>
2024-12-21 15:30:13 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("EMC2 / Cloud Module")
 st.header("App pour test des AI app")
-st.video("https://youtu.be/YfZ0zk5Zzcw")
+#st.video("https://youtu.be/YfZ0zk5Zzcw")
 
 st.sidebar.image("https://seeklogo.com/images/E/ecole-hassania-des-travaux-publics-ehtp-logo-3D5770F217-seeklogo.com.png")
 st.sidebar.header("Master Cloud Computing")
