eb92a27aeca8a3242da2ccdbba14c99bd942e7b8
fadill314 <58606938+fadill314@users.noreply.github.com>
2024-12-21 15:19:18 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -4,4 +4,4 @@ st.title("EMC2 / Cloud Module")
 st.header("App pour test des AI app")
 
 st.sidebar.image("https://lexpress-education.com/wp-content/uploads/2024/08/EHTP.png")
-st.header("Master Cloud Computing")
+st.sidebar.header("Master Cloud Computing")
